45b648ffcfe318cbe58d9b144d62164a4ffe55a7
humera-spec <humera.siddiqua928@gmail.com>
2019-08-06 11:52:29 +0100

    Updated setting.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['thawing-fortress-47076..herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['thawing-fortress-47076.herokuapp.com','127.0.0.1']
 # For example:
 # ALLOWED_HOSTS = ['fathomless-scrubland-30645.herokuapp.com', '127.0.0.1']
 
